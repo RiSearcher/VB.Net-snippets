4fd82304ed7a19334e51f373b1db64643ffa7f18
Sergey Tarasov <tarasov@pmi.lv>
2017-04-26 17:48:24 +0300

    Add "Cell.IsEmpty" extension method.

---
+++ Extensions.vb
@@ -0,0 +1,18 @@
+Imports System.Runtime.CompilerServices
+
+Module Extensions
+
+     <Extension()>
+     Public Function IsEmpty(ByVal cell As DataGridViewCell) As Boolean
+         Return cell.Value Is Nothing OrElse cell.Value Is DBNull.Value 
+OrElse String.IsNullOrWhiteSpace(cell.Value.ToString())
+     End Function
+
+     <Extension()>
+     Public Function IsNotEmpty(ByVal cell As DataGridViewCell) As 
+Boolean
+         Return Not (cell.Value Is Nothing OrElse cell.Value Is 
+DBNull.Value OrElse String.IsNullOrWhiteSpace(cell.Value.ToString()))
+     End Function
+
+End Module
