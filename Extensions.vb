4121ba52cab1012446bc8d4b6dfe0c703a5a8711
Sergey Tarasov <tarasov@pmi.lv>
2017-04-27 00:00:26 +0300

    Update Extensions.vb

--- Extensions.vb
+++ Extensions.vb
@@ -4,15 +4,12 @@ Module Extensions
 
      <Extension()>
      Public Function IsEmpty(ByVal cell As DataGridViewCell) As Boolean
-         Return cell.Value Is Nothing OrElse cell.Value Is DBNull.Value 
-OrElse String.IsNullOrWhiteSpace(cell.Value.ToString())
+         Return cell.Value Is Nothing OrElse cell.Value Is DBNull.Value OrElse String.IsNullOrWhiteSpace(cell.Value.ToString())
      End Function
 
      <Extension()>
-     Public Function IsNotEmpty(ByVal cell As DataGridViewCell) As 
-Boolean
-         Return Not (cell.Value Is Nothing OrElse cell.Value Is 
-DBNull.Value OrElse String.IsNullOrWhiteSpace(cell.Value.ToString()))
+     Public Function IsNotEmpty(ByVal cell As DataGridViewCell) As Boolean
+         Return Not (cell.Value Is Nothing OrElse cell.Value Is DBNull.Value OrElse String.IsNullOrWhiteSpace(cell.Value.ToString()))
      End Function
 
 End Module
